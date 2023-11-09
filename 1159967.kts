3487f168b7a9aaca1cb93d4007f8ddbe289998f7
Lucky Clover <jmj18795967847@gmail.com>
2023-11-09 20:01:50 +0800

    科技花费 + 解锁兵种

--- 1159967.kts
+++ 1159967.kts
@@ -16,6 +16,7 @@ import mindustry.game.EventType
 import mindustry.gen.*
 import mindustry.type.Category
 import mindustry.type.ItemStack
+import mindustry.type.UnitType
 import mindustry.world.Block
 import mindustry.world.blocks.storage.CoreBlock
 
@@ -23,13 +24,13 @@ import mindustry.world.blocks.storage.CoreBlock
 name = "战役模式"
 modeIntroduce(
     "战役模式", "玩法介绍 \n[acid]Made By [vioet]Lucky Clover[]" +
-            "\n类似于战役，你的绝大部分建筑需要使用[cyan]资源[white]解锁\n点击核心进行解锁\n\n[cyan]作图说明[white]\n参考16774简介打上地图标签和设置科技消耗即可"
+            "\n类似于战役，你的绝大部分建筑和兵种需要使用[cyan]资源[white]解锁\n点击核心进行解锁\n\n[cyan]作图说明[white]\n参考16774简介打上地图标签和设置科技消耗即可\n默认倍率(1倍花费)与原版科技相同"
 )
 
-val techCostMultiplier = state.rules.tags.getInt("@techCost", 5)
+val techCostMultiplier = state.rules.tags.getInt("@techCost", 1)
 
-fun Category.name(): String{
-    return when(this){
+fun Category.name(): String {
+    return when (this) {
         Category.crafting -> "加工"
         Category.defense -> "防御"
         Category.distribution -> "物流"
@@ -46,7 +47,7 @@ fun Category.name(): String{
 
 fun Block.resCost(): List<ItemStack> {
     val techCost: MutableList<ItemStack> = mutableListOf()
-    this.requirements.forEach {
+    this.researchRequirements().forEach {
         techCost.add(ItemStack(it.item, it.amount * techCostMultiplier))
     }
     return techCost
@@ -87,10 +88,61 @@ fun Block.buttonName(): String {
     else return "${this.emoji()}\n${this.costName()}"
 }
 
+fun UnitType.unlockTech() {
+    if (this.techNode.parent != null &&
+        ((this.techNode.parent.content is Block && state.rules.bannedBlocks.contains(this.techNode.parent.content as Block)) ||
+                (this.techNode.parent.content is UnitType && state.rules.bannedUnits.contains(this.techNode.parent.content as UnitType)))
+    ) return
+    var canBuild: Boolean = true
+    this.resCost().forEach { if (state.rules.defaultTeam.core().items.get(it.item) < it.amount) canBuild = false }
+    if (!canBuild) return
+    this.resCost().forEach {
+        state.rules.defaultTeam.core().items.remove(it)
+    }
+    state.rules.bannedUnits.remove(this)
+    Call.setRules(state.rules)
+    Call.sendMessage("[acid]已解锁科技：[sky]<Content>[white]${this.emoji()} ${this.name}")
+}
+
+fun UnitType.resCost(): List<ItemStack> {
+    val techCost: MutableList<ItemStack> = mutableListOf()
+    this.researchRequirements().forEach {
+        techCost.add(ItemStack(it.item, it.amount * techCostMultiplier))
+    }
+    return techCost
+}
+
+fun UnitType.costName(): String {
+    if (this.resCost().isEmpty()) return "0元购"
+    var name: String = ""
+    this.resCost().forEach {
+        name += it.item.emoji()
+        name += if (state.rules.defaultTeam.core().items.get(it.item) > it.amount) "[green]" else "[red]"
+        name += "${it.amount}[white] "
+    }
+    return name.dropLast(1)
+}
+
+fun UnitType.buttonName(): String {
+    if (!state.rules.bannedUnits.contains(this)) return "${this.emoji()}[cyan]已解锁"
+    if (this.techNode.parent != null &&
+        ((this.techNode.parent.content is Block && state.rules.bannedBlocks.contains(this.techNode.parent.content as Block)) ||
+                (this.techNode.parent.content is UnitType && state.rules.bannedUnits.contains(this.techNode.parent.content as UnitType)))
+    ) {
+        return "${this.emoji()} \n[gray]前置科技 ${this.techNode.parent.content.emoji()}"
+    }
+    var canBuild: Boolean = true
+    this.resCost().forEach { if (state.rules.defaultTeam.core().items.get(it.item) > it.amount) canBuild = false }
+    if (canBuild) return "${this.emoji()}\n${this.costName()}"
+    else return "${this.emoji()}\n${this.costName()}"
+}
+
+
 val blocksList: Seq<Block> = content.blocks().select { block -> block.isVisible && block.environmentBuildable() }
 
 class CoreMenu(private val player: Player, private val core: CoreBlock.CoreBuild) : MenuBuilder<Unit>() {
     private var category: Category? = null
+    private var buyUnit: Boolean = false
 
     fun sendTo() {
         launch(Dispatchers.game) {
@@ -101,14 +153,23 @@ class CoreMenu(private val player: Player, private val core: CoreBlock.CoreBuild
     private suspend fun mainMenu() {
         msg = "[cyan]科技类别"
         Category.all.forEach {
-            val lsBlockList = blocksList.select{ block -> block.category == it }
+            val lsBlockList = blocksList.select { block -> block.category == it }
             if (lsBlockList.isEmpty) return
-            option("[cyan]${it.name()}[white] ~ ${lsBlockList.first().emoji()}${lsBlockList.get(lsBlockList.size/2) .emoji()}${lsBlockList.last().emoji()}...") {
+            option(
+                "[cyan]${it.name()}[white] ~ ${lsBlockList.first().emoji()}${
+                    lsBlockList.get(lsBlockList.size / 2).emoji()
+                }${lsBlockList.last().emoji()}..."
+            ) {
                 category = it
                 refresh()
             }
             newRow()
         }
+        option("[cyan]兵种[white] ~ ${UnitTypes.mono.emoji()}${UnitTypes.toxopid.emoji()}${UnitTypes.conquer.emoji()}...") {
+            buyUnit = true
+            refresh()
+        }
+        newRow()
     }
 
     private suspend fun unlockMenu() {
@@ -126,12 +187,26 @@ class CoreMenu(private val player: Player, private val core: CoreBlock.CoreBuild
         newRow()
     }
 
+    private suspend fun unlockUnitMenu() {
+        msg =
+            "当前科技类型：[cyan]兵种"
+        var count = 0
+        content.units().select { !it.hidden }.forEach {
+            if (count > 0 && count % 2 == 0) newRow()
+            count += 1
+            option(it.buttonName()) {
+                it.unlockTech();
+                refresh()
+            }
+        }
+        newRow()
+    }
+
     override suspend fun build() {
         title = "[cyan]科技解锁器"
-        when (category) {
-            null -> mainMenu()
-            else -> unlockMenu()
-        }
+        if (buyUnit) unlockUnitMenu()
+        else if (category == null) mainMenu()
+        else unlockMenu()
         //newRow()
         option("[white]退出菜单") { }
     }
